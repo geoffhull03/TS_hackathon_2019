503c0a5fda5a9960b6ce1a4ec876080293e46dca
colinfujiiprocore <32884800+colinfujiiprocore@users.noreply.github.com>
2019-04-25 13:41:59 -0700

    Added env variable for OAUTH URL. Completed handshake & token retrieval

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -7,10 +7,12 @@ class LoginController < ApplicationController
   end
 
   def landing
+    session[:procoreProjectId] = params[:procoreProjectId]
+    session[:procoreCompanyId] = params[:procoreCompanyId]
   end
 
   def procore
-    url = "https://sandbox.procore.com/oauth/authorize?response_type=#{code}&client_id=#{ENV["CLIENT_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}"
+    url = "#{ENV["OAUTH_DOMAIN"]}/oauth/authorize?response_type=code&client_id=#{ENV["CLIENT_ID"]}&redirect_uri=#{ENV["REDIRECT_URI"]}"
 
     redirect_to url
   end
@@ -19,15 +21,17 @@ class LoginController < ApplicationController
     @authorization_code = params["code"]
 
     request = {
-      "grant_type" => "authorization_code",
-      "client_id" => CLIENT_ID,
-      "client_secret" => CLIENT_SECRET,
-      "code" => @authorization_code,
-      "redirect_uri" => REDIRECT_URI
+      grant_type: "authorization_code",
+      client_id: ENV["CLIENT_ID"],
+      client_secret: ENV["CLIENT_SECRET"],
+      code: @authorization_code,
+      redirect_uri: ENV["REDIRECT_URI"]
     }
 
-    response = RestClient.post(OAUTH_URL + '/oauth/token', request.to_json, {content_type: :json, accept: :json})
-      session[:oauth_response] = JSON.parse(response)
+    response = RestClient.post("#{ENV["OAUTH_DOMAIN"]}/oauth/token", request.to_json, {content_type: :json, accept: :json})
+    session[:oauth_response] = JSON.parse(response)
+    puts session[:oauth_response]
 
+    redirect_to root_path
   end
 end
