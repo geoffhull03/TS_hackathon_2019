6a68f825bef4e8dbf8c730e567b76b2bf04e76f3
colinfujiiprocore <32884800+colinfujiiprocore@users.noreply.github.com>
2019-04-26 14:18:51 -0700

    Refactored nav header into view object. Implemented view object helper method. removed puts statements from POC/WOC controllers. Moved debug code in those controllers to unless loops to prevent overwriting in prod env

--- app/controllers/purchase_order_contracts_controller.rb
+++ app/controllers/purchase_order_contracts_controller.rb
@@ -2,15 +2,19 @@ class PurchaseOrderContractsController < ApplicationController
 	before_action :logged_in
 
   def index
-    session[:procoreProjectId] = '8673'
-    session[:procoreCompanyId] = '13958'
+  	# Unless loops to prevent debug code from triggering in prod
+  	unless session[:procoreProjectId]
+    	session[:procoreProjectId] = '8673'
+    end
+    unless session[:procoreCompanyId]
+    	session[:procoreCompanyId] = '13958'
+    end
 
     list_poc= RestClient.get(ENV['BASE_URL'] + '/vapid/purchase_order_contracts?project_id=' + session[:procoreProjectId],
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
     "Procore-Company-ID" => session[:procoreCompanyId]})
 
     @purchase_order_contracts = JSON.parse(list_poc)
-    puts @purchase_order_contracts
   end
 
 end
--- app/controllers/work_order_contracts_controller.rb
+++ app/controllers/work_order_contracts_controller.rb
@@ -2,14 +2,18 @@ class WorkOrderContractsController < ApplicationController
 	before_action :logged_in
 
   def index
-    session[:procoreProjectId] = '8673'
-    session[:procoreCompanyId] = '13958'
+    # Debug values in unless to avoid trigger in Prod
+    unless session[:procoreProjectId]
+      session[:procoreProjectId] = '8673'
+    end
+    unless session[:procoreCompanyId]
+      session[:procoreCompanyId] = '13958'
+    end
 
     list_woc= RestClient.get("#{ENV['BASE_URL']}/vapid/work_order_contracts?project_id=#{session[:procoreProjectId]}",
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
     "Procore-Company-ID" => session[:procoreCompanyId]})
 
     @work_order_contracts = JSON.parse(list_woc)
-    puts @work_order_contracts
   end
 end
