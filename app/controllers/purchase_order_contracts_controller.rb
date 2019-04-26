b736d655b34c1056cc8137f2d19fcb6b144adf12
Nick Morrill <nick.morrill@procore.com>
2019-04-26 14:21:13 -0700

    Updated URL strings in work_order_contracts and purchase_order_contracts controllers and converted session variables to being set conditionally

--- app/controllers/purchase_order_contracts_controller.rb
+++ app/controllers/purchase_order_contracts_controller.rb
@@ -1,20 +1,19 @@
 class PurchaseOrderContractsController < ApplicationController
 
   def index
-    session[:procoreProjectId] = '8673'
-    session[:procoreCompanyId] = '13958'
+    session[:procoreProjectId] ||= '8673'
+    session[:procoreCompanyId] ||= '13958'
 
     if params[:search]
       query = "&filters[origin_id]=" + "#{params[:search]}"
     else query = ""
     end
     puts
-    list_poc= RestClient.get(ENV['BASE_URL'] + '/vapid/purchase_order_contracts?project_id=' + session[:procoreProjectId] + query,
+    list_poc= RestClient.get("#{ENV['BASE_URL']}/vapid/purchase_order_contracts?project_id=#{session[:procoreProjectId]}#{query}",
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
     "Procore-Company-ID" => session[:procoreCompanyId]})
 
     @purchase_order_contracts = JSON.parse(list_poc)
-    puts @purchase_order_contracts
   end
 
 end
--- app/controllers/work_order_contracts_controller.rb
+++ app/controllers/work_order_contracts_controller.rb
@@ -1,21 +1,21 @@
 class WorkOrderContractsController < ApplicationController
 
   def index
-    session[:procoreProjectId] = '8673'
-    session[:procoreCompanyId] = '13958'
+    session[:procoreProjectId] ||= '8673'
+    session[:procoreCompanyId] ||= '13958'
 
       if params[:search]
         query = "&filters[origin_id]=" + "#{params[:search]}"
-      else query = ""
+      else 
+      	query = ""
       end
 
-    list_woc= RestClient.get(ENV['BASE_URL'] + '/vapid/work_order_contracts?project_id=' + session[:procoreProjectId] + query,
+    list_woc= RestClient.get("#{ENV['BASE_URL']}/vapid/work_order_contracts?project_id=#{session[:procoreProjectId]}#{query}",
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
     "Procore-Company-ID" => session[:procoreCompanyId]})
 
 
     @work_order_contracts = JSON.parse(list_woc)
-    puts @work_order_contracts
   end
 
 end
