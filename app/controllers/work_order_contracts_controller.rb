d38ece125c58dc1258d058df74e267ad2f7d9a31
Nick Morrill <nick.morrill@procore.com>
2019-04-26 11:13:54 -0700

    Modified URL string in work order contracts controller to use string interpolation

--- app/controllers/work_order_contracts_controller.rb
+++ app/controllers/work_order_contracts_controller.rb
@@ -4,7 +4,7 @@ class WorkOrderContractsController < ApplicationController
     session[:procoreProjectId] = '8673'
     session[:procoreCompanyId] = '13958'
 
-    list_woc= RestClient.get(ENV['BASE_URL'] + '/vapid/work_order_contracts?project_id=' + session[:procoreProjectId],
+    list_woc= RestClient.get("#{ENV['BASE_URL']}/vapid/work_order_contracts?project_id=#{session[:procoreProjectId]}",
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
     "Procore-Company-ID" => session[:procoreCompanyId]})
 
