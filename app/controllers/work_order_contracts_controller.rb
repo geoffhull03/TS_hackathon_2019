3ac149995202bc1d2e6e9c07ac413a577b99fba6
Nick Morrill <nick.morrill@procore.com>
2019-04-26 12:03:12 -0700

    Added new work order contracts index view with core components for the table. Added sortTable.js library to assets/shared/ directory. Added conditional operator to session var assignments in WorkOrderContractsController#index method. Fixed bug in formatting of application.html.erb

--- app/controllers/work_order_contracts_controller.rb
+++ app/controllers/work_order_contracts_controller.rb
@@ -1,8 +1,8 @@
 class WorkOrderContractsController < ApplicationController
 
   def index
-    session[:procoreProjectId] = '8673'
-    session[:procoreCompanyId] = '13958'
+    session[:procoreProjectId] ||= '8673'
+    session[:procoreCompanyId] ||= '13958'
 
     list_woc= RestClient.get("#{ENV['BASE_URL']}/vapid/work_order_contracts?project_id=#{session[:procoreProjectId]}",
     {"Authorization" => "Bearer #{session[:oauth_response]['access_token']}",
